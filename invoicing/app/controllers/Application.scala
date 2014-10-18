13f7105e0b3402375cfc1da68d614381fd75dced
george <ga111122222@gmail.com>
2014-10-18 14:12:09 -0700

    formatting

--- invoicing/app/controllers/Application.scala
+++ invoicing/app/controllers/Application.scala
@@ -2,33 +2,22 @@ package controllers
 
 import java.io.File
 
+import play.Play.application
 import play.api.mvc.{Action, Controller}
+
+import scala.io.Source.fromFile
 
 object Application extends Controller {
 
-  def main(s: String) = Action {
-    Ok(views.html.main())
-  }
+  def main(s: String) = Action { Ok(views.html.main()) }
 
-  def getURI(any: String): String = any match {
-    case s: String => "/public/partials/" + s + ".html"
-  }
+  def getURI(any: String) = any match { case s => "/public/partials/" + s + ".html" }
 
   def loadPublicHTML(any: String) = Action {
-    import play.Play
-    val projectRoot = Play.application.path
-    import java.io.File
+    val projectRoot = application.path
     val file = new File(projectRoot + getURI(any))
-    file.exists match {
-      case true => Ok(scala.io.Source.fromFile(file.getCanonicalPath).mkString) as "text/html"
-      case _ => NotFound
-    }
+    file.exists match { case true => Ok(fromFile(file getCanonicalPath) mkString) as "text/html" case _ => NotFound }
   }
 
-  def getPdf = Action {
-    Ok sendFile(
-      content = new File("/home/george/books/algorithms.pdf"),
-      inline = true
-    )
-  }
+  def getPdf = Action { Ok sendFile(new File("/home/george/books/algorithms.pdf"), true) }
 }
