e87ee097beb0a09892190738dba89b1e0903b64e
george <ga111122222@gmail.com>
2014-10-20 22:44:04 -0700

    ui bootstrap datepicker

--- invoicing/app/assets/javascripts/controllers.js
+++ invoicing/app/assets/javascripts/controllers.js
@@ -90,8 +90,39 @@ controllers.controller('InvoiceCtrl', ['$scope', '$resource', '$routeParams', '$
     };
     $scope.getTaxRates();
 
-    var date = new Date();
-    $scope.today = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
     $resource(apiUrl + '/lineitems/create')
         .get()
         .$promise
